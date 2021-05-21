ccca15388b3a0977b44f52140273406bc1cd0e11
mgpijuan <mgpijuan@gmail.com>
2021-05-21 13:04:23 +0200

    api

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-fetch(url)
+const p = document.getElementById('joke');
+const url = 'https://icanhazdadjoke.com/';
+
+
+fetch("https://icanhazdadjoke.com/");
+.then(function(){
+
+})
+.catch(function() {
+
+});
+
